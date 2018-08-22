0c148950e93a1c4235b52dc6b1be41a37d5141ff
Vadim <pharshmaster@gmail.com>
2018-08-22 17:18:09 +0300

    better check plugins && is mobile (#2)

--- index.js
+++ index.js
@@ -8,26 +8,35 @@ function hasPhantomStacktrace() {
     }
 }
 
+function hasNoPlugins() {
+    if (!(navigator.plugins instanceof PluginArray) || navigator.plugins.length === 0) {
+        return true;
+    }
+
+    return [].slice.call(navigator.plugins).reduce((count, current) => {
+        return current instanceof Plugin ? count + 1 : count;
+    }, 0) === 0;
+}
+
 function isHeadless(inconsistentPermissionsState) {
     const userAgent = window && window.navigator && window.navigator.userAgent || '';
     const noUserAgent = !userAgent;
+    const isMobile = /(iphone|android|(windows phone))/i.test(userAgent);
     const isHeadlessChrome = /HeadlessChrome/i.test(userAgent);
     const isChrome = /(chrome|chromium)/i.test(userAgent);
     const isOpera = /opera/i.test(userAgent);
     const isWebdriver = !!navigator.webdriver;
     const isPhantom = window.callPhantom || window._phantom || hasPhantomStacktrace();
     const noChromeProperty = !window.chrome;
-    const noPlugins = !navigator.plugins.length;
-    const noLanguages = !navigator.languages;
+    const noPlugins = hasNoPlugins();
 
     return (
         noUserAgent ||
-        noLanguages ||
         isHeadlessChrome ||
         isWebdriver ||
         isPhantom ||
-        ((isChrome || isOpera) && noChromeProperty) ||
-        (isChrome && noPlugins) ||
+        ((isChrome || isOpera) && noChromeProperty && !isMobile) ||
+        (isChrome && noPlugins && !isMobile) ||
         inconsistentPermissionsState
     );
 }
